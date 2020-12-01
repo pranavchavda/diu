4a94db718f8e4450af1ceb658d384e935c46d47d
Pranav <pranav@idrinkcoffee.com>
2020-12-01 16:29:10 -0500

    errorfix

--- static/form.php
+++ static/form.php
@@ -89,6 +89,8 @@ $form->addPlugin('formvalidation', '#room-booking-form');
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -121,7 +123,6 @@ $form->addPlugin('formvalidation', '#room-booking-form');
 
     <!-- jQuery -->
 
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
     <!-- Bootstrap 4 JavaScript -->
 
